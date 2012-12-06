2fbfafb64204f2d4b39c67b61f33f10f10ac5809
Andrew Hall <icymint3@gmail.com>
2012-12-06 15:14:07 -0500

    using handlebars for templates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+		var self = this;
+		this.store.findByName( $('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -19,11 +13,21 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
     initialize: function() {
         var self = this;
-        this.store = new LocalStorageStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+		
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
+		this.store = new LocalStorageStore(function() {
+            //self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
         });
+		
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
