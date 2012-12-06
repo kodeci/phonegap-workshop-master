6d45136d149b4cee137bd0a6485ddc6c3f26f13e
Andrew Hall <icymint3@gmail.com>
2012-12-06 15:55:47 -0500

    styling, scrolling and events

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 var app = {
-
-    findByName: function() {
-		var self = this;
-		this.store.findByName( $('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -13,24 +6,33 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	registerEvents: function() {
+		var self = this;
+		// Check of browser supports touch events...
+		if (document.documentElement.hasOwnProperty('ontouchstart')) {
+			// ... if yes: register touch event listener to change the "selected" state of the item
+			$('body').on('touchstart', 'a', function(event) {
+				$(event.target).addClass('tappable-active');
+			});
+			$('body').on('touchend', 'a', function(event) {
+				$(event.target).removeClass('tappable-active');
+			});
+		} else {
+			// ... if not: register mouse events instead
+			$('body').on('mousedown', 'a', function(event) {
+				$(event.target).addClass('tappable-active');
+			});
+			$('body').on('mouseup', 'a', function(event) {
+				$(event.target).removeClass('tappable-active');
+			});
+		}
 	},
-    initialize: function() {
-        var self = this;
-		
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        
+	initialize: function() {
+		var self = this;
 		this.store = new LocalStorageStore(function() {
-            //self.showAlert('Store Initialized', 'Info');
-			self.renderHomeView();
-        });
-		
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+			$('body').html( new HomeView(self.store).render().el );
+		});
+	}
 };
 
 app.initialize();
